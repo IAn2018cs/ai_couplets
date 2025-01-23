136c739a45d8640d23c2d84d92e9467a26b63899
Timothy Yin <master@uniiem.com>
2025-01-23 14:02:21 +0800

    feat: adjust the prompt

--- utils/openai.ts
+++ utils/openai.ts
@@ -7,19 +7,17 @@ const openai = new OpenAI({
 });
 
 export const generateCouplet = async (prompt: string): Promise<Couplet> => {
-  // TODO: configure max input length
-  prompt = prompt.trim().slice(0, 100);
+  prompt = prompt.trim().slice(0, parseInt(process.env.MAX_TOKENS || "100"));
 
   const systemPrompt = `
-根据我的提示生成一副春联，要求包含上、下联，每句话字数在五到十二字之间，且两句字数相同，然后根据上下联给出不超过五个字的横批，同时总结全文给出一个字的总结。
-要求：不能出现任何标点符号、不使用生僻字。按照下面给出的格式输出，确保可以被 JSON.parse 正确解析，注意全部使用半角符号。不要有任何多余的解释内容。
-格式如下：
+根据我的提示生成一副春联，要求包含上、下联，每句话需保持五至十二字，且上下联字数必须一致，然后根据上下联给出不超过五个字的横批，同时总结全文给出一个字的总结。不能出现任何标点符号，不使用生僻字，并按照下面格式输出，确保可以被JSON.parse正确解析，注意全部使用半角符号，不要有任何多余的解释内容。
 {
   "上联": "",
   "下联": "",
   "横批": "",
   "总结": ""
-}`;
+}
+`;
 
   const presetMessages: OpenAI.ChatCompletionMessageParam[] = [
     {
@@ -36,7 +34,7 @@ export const generateCouplet = async (prompt: string): Promise<Couplet> => {
   }
 
   const completion = await openai.chat.completions.create({
-    max_tokens: 100,
+    max_tokens: parseInt(process.env.MAX_TOKENS || "100"),
     messages: presetMessages,
     model: process.env.OPENAI_MODEL || "deepseek-chat",
     temperature: 1.5,
